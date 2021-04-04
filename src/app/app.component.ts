7b4e2237e58c30262eb531492d0e201134218dbe
Edjaine <c83edsoliveiraxxx@gmail.com>
2021-04-04 14:39:26 -0300

    Implementação do envio de arquivos ao blob storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,10 @@ import { BlobService } from './blob.service';
 
 export class AppComponent {
 
+  sucesso1 = false;
+  sucesso2 = false;
+  sucesso3 = false;
+
   title = 'app-blob';  
   formUpload = this.formBuilder.group({
     arquivo1: '',
@@ -21,16 +25,18 @@ export class AppComponent {
   constructor(private formBuilder: FormBuilder,
               private blobService: BlobService){ }
 
-  public enviaArquivo1(): void{    
+  public async enviaArquivo1(){    
     var file = this.formUpload.get("arquivo1")?.value
-    this.blobService.adiciona(file.files[0], file.files[0].name);
+    this.sucesso1 = await this.blobService.adiciona(file.files[0], file.files[0].name);
   }
 
-  public enviaArquivo2(): void{
-    console.log("enviou arquivo 2")
+  public async enviaArquivo2(){
+    var file = this.formUpload.get("arquivo2")?.value
+    this.sucesso2 = await this.blobService.adiciona(file.files[0], file.files[0].name);
   }
 
-  public enviaArquivo3(): void{
-    console.log("enviou arquivo 3")
+  public async enviaArquivo3() {
+    var file = this.formUpload.get("arquivo3")?.value
+    this.sucesso3 = await this.blobService.adiciona(file.files[0], file.files[0].name);
   }
 }
--- src/app/blob.service.ts
+++ src/app/blob.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BlobServiceClient, ContainerClient } from '@azure/storage-blob';
+import { BlobServiceClient, BlockBlobUploadResponse, ContainerClient } from '@azure/storage-blob';
 @Injectable({
   providedIn: 'root'
 })
@@ -11,11 +11,11 @@ export class BlobService {
         
     const containerName = 'poc-container';
 
-    this.client = new BlobServiceClient("https://stgpocestudo.blob.core.windows.net/?sv=2020-02-10&ss=bfqt&srt=sco&sp=rwdlacupx&se=2022-04-04T23:54:11Z&st=2021-04-04T15:54:11Z&spr=https&sig=lIc93Y9B9lcaZIW9fDFxlK4EN3XW2%2FCQap59uRlm9u4%3D");
+    this.client = new BlobServiceClient("Insira aqui a Share Acess Signature");
     this.containerClient = this.client.getContainerClient(containerName);
   }
 
-  public async adiciona(arquivo: any, nome: string){
+  public async adiciona(arquivo: any, nome: string) : Promise<boolean> {
     
     console.log("enviado para azure");    
     
@@ -24,6 +24,12 @@ export class BlobService {
     const response = await blockBlobClient.upload(arquivo, arquivo.size);
 
     console.log(response);
-    return response;
+
+    if(response._response.status === 201)
+      return true;
+
+    return false;
+
+    
   }
 }
